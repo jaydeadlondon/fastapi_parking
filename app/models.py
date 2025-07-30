d4e13dc44758d6136bb497b5f03287f81358c887
jaydeadlondon <ivanstetsenko48@gmail.com>
2025-07-31 02:42:51 +0300

    init project with CI

---
+++ app/models.py
@@ -0,0 +1,40 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Client(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    surname = db.Column(db.String(50), nullable=False)
+    credit_card = db.Column(db.String(50))
+    car_number = db.Column(db.String(10))
+
+    def __repr__(self):
+        return f"<Client {self.name} {self.surname}>"
+
+class Parking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String(100), nullable=False)
+    opened = db.Column(db.Boolean)
+    count_places = db.Column(db.Integer, nullable=False)
+    count_available_places = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"<Parking {self.address}>"
+
+class ClientParking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    client_id = db.Column(db.Integer, db.ForeignKey('client.id'))
+    parking_id = db.Column(db.Integer, db.ForeignKey('parking.id'))
+    time_in = db.Column(db.DateTime)
+    time_out = db.Column(db.DateTime)
+
+    client = db.relationship('Client', backref='client_parkings')
+    parking = db.relationship('Parking', backref='client_parkings')
+
+    __table_args__ = (
+        db.UniqueConstraint('client_id', 'parking_id', name='unique_client_parking'),
+    )
+
+    def __repr__(self):
+        return f"<ClientParking client={self.client_id} parking={self.parking_id}>"
---
+++ tests/factories.py
@@ -0,0 +1,24 @@
+import factory
+from faker import Faker
+from module_30_ci_linters.homework.hw1.fastapi_parking.app.models import Client, Parking
+import random
+
+fake = Faker('ru_RU')
+
+class ClientFactory(factory.Factory):
+    class Meta:
+        model = Client
+
+    name = factory.LazyAttribute(lambda x: fake.first_name())
+    surname = factory.LazyAttribute(lambda x: fake.last_name())
+    credit_card = factory.LazyAttribute(lambda x: fake.credit_card_number() if random.choice([True, False]) else None)
+    car_number = factory.LazyAttribute(lambda x: fake.bothify(text='?###??##'))
+
+class ParkingFactory(factory.Factory):
+    class Meta:
+        model = Parking
+
+    address = factory.LazyAttribute(lambda x: fake.address())
+    opened = factory.LazyAttribute(lambda x: random.choice([True, False]))
+    count_places = factory.LazyAttribute(lambda x: random.randint(1, 100))
+    count_available_places = factory.LazyAttribute(lambda o: o.count_places)
