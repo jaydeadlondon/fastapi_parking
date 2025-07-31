a4051f33d3c9764b528faf448df5178ad9854792
jaydeadlondon <ivanstetsenko48@gmail.com>
2025-07-31 03:01:55 +0300

    updated

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,11 @@ from datetime import datetime
 import pytest
 from module_30_ci_linters.homework.hw1.fastapi_parking.app import create_app
 from module_30_ci_linters.homework.hw1.fastapi_parking.app.models import (
-    Client, ClientParking, Parking)
-from module_30_ci_linters.homework.hw1.fastapi_parking.app.models import \
-    db as _db
+    Client,
+    ClientParking,
+    Parking,
+)
+from module_30_ci_linters.homework.hw1.fastapi_parking.app.models import db as _db
 
 
 @pytest.fixture
